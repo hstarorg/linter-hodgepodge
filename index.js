8050df1a903e3648ca42c65df60d0ded39301067
Jay.M.Hu <jay.m.hu@newegg.com>
2016-07-02 17:34:09 +0800

    Project init.

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  JsHintReporter: require('./lib/jshint/jshintReporter')
+};
---
+++ test/jshint.test.js
@@ -0,0 +1,3 @@
+'use strict';
+
+let JsHintReporter = require('./../index').JsHintReporter;
